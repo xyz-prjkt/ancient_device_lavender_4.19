5c5b56bc070cc3210569a6946e033ee513fa08a5
alanndz <alanmahmud0@gmail.com>
2020-12-07 19:05:15 +0000

    sdm660-common: remove aosip overlay and dependencies
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -27,8 +27,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
