b260f9b47d35d01465e236867887afad302d9df9
xyzuan <i.xyzuan@gmail.com>
2021-02-05 16:41:21 +0700

    lavender: revert ant, bluetooth blobs update from laurel_sprout

--- device.mk
+++ device.mk
@@ -172,9 +172,10 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService-Soong \
+    AntHalService \
+    antradio_app \
     com.dsi.ant.antradio_library \
-    com.dsi.ant@1.0.vendor
+    libantradio
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
@@ -195,8 +196,7 @@ PRODUCT_BOOT_JARS += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
